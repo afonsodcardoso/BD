36d770c9c473b5e5e285348bf3f230fc03f18a31
afonsodcardoso <afonsocardoso@ua.pt>
2022-03-20 01:02:04 +0000

    Guiao1 Completed

--- Aula1/Aula1/Aula1/Form2.cs
+++ Aula1/Aula1/Aula1/Form2.cs
@@ -7,16 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Aula1
 {
     public partial class Form2 : Form
     {
+        SqlConnection CN;
         public Form2()
         {
             InitializeComponent();
         }
-        private void UpdateTextPosition()
+        private void UpdateTextPosition()  //código para centrar o texto título do window retirado do stackoverflow
         {
             Graphics g = this.CreateGraphics();
             Double startingPoint = (this.Width / 2) - (g.MeasureString(this.Text.Trim(), this.Font).Width / 2);
@@ -33,6 +35,63 @@ namespace Aula1
             this.Text = tmp + this.Text.Trim();
         }
 
+        private void TestDBConnection(String dbServer, String dbName, String userName, String userPass)
+        {
+            CN = new SqlConnection("Data Source = " + dbServer + " ;" + "Initial Catalog = " + dbName + "; uid = " + userName + ";" + "password = " + userPass);
+            try
+            {
+                CN.Open();
+                if (CN.State == ConnectionState.Open)
+                {
+                    MessageBox.Show("Successful connection to database " + CN.Database + " on the " + CN.DataSource + " server", "Connection Test", MessageBoxButtons.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("FAILED TO OPEN CONNECTION TO DATABASE DUE TO THE FOLLOWING ERROR" + ex.Message, "Connection Test", MessageBoxButtons.OK);
+                throw;
+            }
+
+            if (CN.State == ConnectionState.Open)
+            {
+                CN.Close();
+            }
+        }
+
+        private string getTableContent(SqlConnection CN)
+        {
+            string str = "";
+
+            try
+            {
+                CN.Open();
+                if (CN.State == ConnectionState.Open)
+                {
+                    int cnt = 1;
+                    SqlCommand sqlcmd = new SqlCommand("SELECT * FROM Hello", CN);
+                    SqlDataReader reader;
+                    reader = sqlcmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        str += cnt.ToString() + " - " + reader.GetInt32(reader.GetOrdinal("MsgID")) + ", ";
+                        str += reader.GetString(reader.GetOrdinal("MsgSubject"));
+                        str += "\n";
+                        cnt += 1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("FAILED TO OPEN CONNECTION TO DATABASE DUE TO THE FOLLOWING ERROR \r\n" + ex.Message, "Connection Error", MessageBoxButtons.OK);
+            }
+
+            if (CN.State == ConnectionState.Open)
+                CN.Close();
+
+            return str;
+        }
+
         private void Form2_Load(object sender, EventArgs e)
         {
             UpdateTextPosition();
@@ -41,6 +100,26 @@ namespace Aula1
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TestDBConnection(textBox1.Text, textBox2.Text, textBox2.Text, textBox3.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(getTableContent(CN), "Dump Hello Table",MessageBoxButtons.OK);
         }
     }
 }
--- Aula1/Aula1/Aula1/Program.cs
+++ Aula1/Aula1/Aula1/Program.cs
@@ -16,7 +16,7 @@ namespace Aula1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
